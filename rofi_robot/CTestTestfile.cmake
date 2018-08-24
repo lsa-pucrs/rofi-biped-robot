2dd59b126b2d95e76d0450f2f9b5b2beac10b825
Rafael Winck <rafael.winck@acad.pucrs.br>
2018-08-24 14:49:50 -0300

    removing compiled files

--- rofi_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/catkin_ws/src/rofi_robot
-# Build directory: /home/pi/catkin_ws/build/rofi_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
